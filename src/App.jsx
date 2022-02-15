3b42012c1957cc00ba31146c3b65c299cef2d881
MinhNguyen <minhnv@reactplus.jp>
2022-02-15 13:59:55 +0700

    detect production enviroment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import "antd/dist/antd.css";
+import { useEffect } from "react";
 import FilterContainer from "./components/filter/filterContainer";
 import { Container } from "./components/styled/container.styled";
 import InputAddTodo from "./components/todoList/inputAddTodo";
 import TodoList from "./components/todoList/todoList";
 
 function App() {
+  useEffect(() => {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+      // development build code
+      console.log("running on development")
+  } else {
+      // production build code
+      console.log("running on product")
 
+  }
+  }, [])
   return (
     <div className="App">
       <Container>
